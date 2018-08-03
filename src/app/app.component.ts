c64835341c1a27c70b91b806852f7200a856fbe7
Bruno Staub <bruno.staub@gmail.com>
2018-08-03 19:20:05 +0200

    Video 26 end fix admin navbar!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  get front() {
+  front() {
     if (localStorage.getItem('user') === '\"admin\"'){
       return false;
     }
